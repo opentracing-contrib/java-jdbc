591258fd11c3e8306e6aeea9dbd7bb1e441665cc
Sergei Malafeev <sergeymalafeev@gmail.com>
2018-08-29 22:26:35 +0800

    update deps, add plain jdbc test
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ src/test/java/io/opentracing/contrib/jdbc/JdbcTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.opentracing.contrib.jdbc;
+
+import static org.junit.Assert.assertEquals;
+
+import io.opentracing.mock.MockSpan;
+import io.opentracing.mock.MockTracer;
+import io.opentracing.util.GlobalTracerTestUtil;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.List;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JdbcTest {
+
+  private static final MockTracer mockTracer = new MockTracer();
+
+  @BeforeClass
+  public static void init() {
+    GlobalTracerTestUtil.setGlobalTracerUnconditionally(mockTracer);
+  }
+
+  @Before
+  public void before() {
+    mockTracer.reset();
+  }
+
+  @Test
+  public void test() throws Exception {
+    Connection connection = DriverManager.getConnection("jdbc:tracing:h2:mem:jdbc");
+    Statement statement = connection.createStatement();
+    statement.executeUpdate("CREATE TABLE employer (id INTEGER)");
+    connection.close();
+
+    List<MockSpan> spans = mockTracer.finishedSpans();
+    assertEquals(1, spans.size());
+  }
+
+}
