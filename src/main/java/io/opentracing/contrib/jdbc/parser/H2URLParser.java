5e8eac250c343f0982119a31ee782d89f20c4214
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-06 21:53:22 +0800

    #33 fix parsing H2 URL with implicit file mode
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/main/java/io/opentracing/contrib/jdbc/parser/H2URLParser.java
+++ src/main/java/io/opentracing/contrib/jdbc/parser/H2URLParser.java
@@ -20,13 +20,17 @@ public class H2URLParser extends AbstractURLParser {
   private static final String LOCALHOST = "localhost";
   private static final int DEFAULT_PORT = 8084;
   /**
+   * Flag that H2 running with memory mode.
+   */
+  private static final String MEMORY_MODE_FLAG = "mem";
+  /**
    * Flag that H2 running with file mode.
    */
   private static final String FILE_MODE_FLAG = "file";
   /**
-   * Flag that H2 running with memory mode.
+   * Flag that H2 running with implicit file mode.
    */
-  private static final String MEMORY_MODE_FLAG = "mem";
+  private static final String IMPLICIT_FILE_MODE_FLAG = "jdbc:h2";
   private static final String H2_DB_TYPE = "h2";
 
   @Override
@@ -60,6 +64,12 @@ public class H2URLParser extends AbstractURLParser {
           .dbInstance(fetchDatabaseNameFromURL(url, databaseNameRangeIndex)).build();
     }
 
+    databaseNameRangeIndex = fetchDatabaseNameRangeIndexFromURLForH2ImplicitFileMode(url);
+    if (databaseNameRangeIndex != null) {
+      return new ConnectionInfo.Builder(LOCALHOST, -1).dbType(H2_DB_TYPE)
+          .dbInstance(fetchDatabaseNameFromURL(url, databaseNameRangeIndex)).build();
+    }
+
     String[] hostAndPort = fetchDatabaseHostsFromURL(url).split(":");
     if (hostAndPort.length == 1) {
       return new ConnectionInfo.Builder(hostAndPort[0], DEFAULT_PORT).dbType(H2_DB_TYPE)
@@ -85,6 +95,26 @@ public class H2URLParser extends AbstractURLParser {
 
     if (fileLabelIndex != -1) {
       return new int[]{fileLabelIndex + FILE_MODE_FLAG.length() + 1, parameterLabelIndex};
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Fetch range index that the database name from connection url if H2 database running with
+   * implicit file mode.
+   *
+   * @return range index that the database name.
+   */
+  private int[] fetchDatabaseNameRangeIndexFromURLForH2ImplicitFileMode(String url) {
+    int fileLabelIndex = url.indexOf(IMPLICIT_FILE_MODE_FLAG);
+    int parameterLabelIndex = url.indexOf(";", fileLabelIndex);
+    if (parameterLabelIndex == -1) {
+      parameterLabelIndex = url.length();
+    }
+
+    if (fileLabelIndex != -1) {
+      return new int[]{fileLabelIndex + IMPLICIT_FILE_MODE_FLAG.length() + 1, parameterLabelIndex};
     } else {
       return null;
     }
