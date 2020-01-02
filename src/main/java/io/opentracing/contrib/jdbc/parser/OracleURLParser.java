dfb8f3e98eb95ea5d518493b51ed9d7687b28cc8
Sergei Malafeev <sergeymalafeev@gmail.com>
2020-01-02 21:02:25 +0800

    update year
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/main/java/io/opentracing/contrib/jdbc/parser/OracleURLParser.java
+++ src/main/java/io/opentracing/contrib/jdbc/parser/OracleURLParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 The OpenTracing Authors
+ * Copyright 2017-2020 The OpenTracing Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -13,140 +13,140 @@
  */
 package io.opentracing.contrib.jdbc.parser;
 
+import io.opentracing.contrib.jdbc.ConnectionInfo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import io.opentracing.contrib.jdbc.ConnectionInfo;
-
 public class OracleURLParser implements ConnectionURLParser {
-    public static final String DB_TYPE = "oracle";
-    public static final String PREFIX = "jdbc:oracle:thin:";
-    public static final int DEFAULT_PORT = 1521;
-    private static Pattern EASY_CONNECT_PATTERN = Pattern.compile(
-        "(?<username>.*)@(//)?(?<host>[^:/]+)(?<port>:[0-9]+)?(?<service>[:/][^:/]+)?(?<server>:[^:/]+)?(?<instance>/[^:/]+)?");
+  public static final String DB_TYPE = "oracle";
+  public static final String PREFIX = "jdbc:oracle:thin:";
+  public static final int DEFAULT_PORT = 1521;
+  private static Pattern EASY_CONNECT_PATTERN = Pattern.compile(
+      "(?<username>.*)@(//)?(?<host>[^:/]+)(?<port>:[0-9]+)?(?<service>[:/][^:/]+)?(?<server>:[^:/]+)?(?<instance>/[^:/]+)?");
 
-    @Override
-    public ConnectionInfo parse(final String url) {
-        if (url != null && url.startsWith(PREFIX)) {
-            OracleConnectionInfo connectionInfo = parseTnsName(url.substring(PREFIX.length()));
-            if (connectionInfo == null) {
-                connectionInfo = parseEasyConnect(url.substring(PREFIX.length()));
-            }
-            if (connectionInfo != null) {
-                return new ConnectionInfo.Builder(connectionInfo.getDbPeer()) //
-                    .dbType(DB_TYPE) //
-                    .dbInstance(connectionInfo.getDbInstance()) //
-                    .build();
-            }
-        }
-        return null;
+  @Override
+  public ConnectionInfo parse(final String url) {
+    if (url != null && url.startsWith(PREFIX)) {
+      OracleConnectionInfo connectionInfo = parseTnsName(url.substring(PREFIX.length()));
+      if (connectionInfo == null) {
+        connectionInfo = parseEasyConnect(url.substring(PREFIX.length()));
+      }
+      if (connectionInfo != null) {
+        return new ConnectionInfo.Builder(connectionInfo.getDbPeer()) //
+            .dbType(DB_TYPE) //
+            .dbInstance(connectionInfo.getDbInstance()) //
+            .build();
+      }
+    }
+    return null;
+  }
+
+  private OracleConnectionInfo parseTnsName(final String url) {
+    final String hosts = parseDatabaseHostsFromTnsUrl(url);
+    if (hosts != null) {
+      final int idxServiceName = url.indexOf("SERVICE_NAME");
+      final int start = url.indexOf('=', idxServiceName) + 1;
+      final int end = url.indexOf(")", start);
+      final String serviceName = url.substring(start, end);
+      return new OracleConnectionInfo() //
+          .setDbPeer(hosts) //
+          .setDbInstance(serviceName);
+    }
+    return null;
+  }
+
+  public static String parseDatabaseHostsFromTnsUrl(String url) {
+    int beginIndex = url.indexOf("DESCRIPTION");
+    if (beginIndex == -1) {
+      return null;
+    }
+    List<String> hosts = new ArrayList<String>();
+    do {
+      int hostStartIndex = url.indexOf("HOST", beginIndex);
+      if (hostStartIndex == -1) {
+        break;
+      }
+      int equalStartIndex = url.indexOf("=", hostStartIndex);
+      int hostEndIndex = url.indexOf(")", hostStartIndex);
+      String host = url.substring(equalStartIndex + 1, hostEndIndex);
+
+      int port = DEFAULT_PORT;
+      int portStartIndex = url.indexOf("PORT", hostEndIndex);
+      int portEndIndex = url.length();
+      if (portStartIndex != -1) {
+        int portEqualStartIndex = url.indexOf("=", portStartIndex);
+        portEndIndex = url.indexOf(")", portEqualStartIndex);
+        port = Integer.parseInt(url.substring(portEqualStartIndex + 1, portEndIndex).trim());
+      }
+      hosts.add(host.trim() + ":" + port);
+      beginIndex = portEndIndex;
+    } while (true);
+    return join(",", hosts);
+  }
+
+  private static String join(String delimiter, List<String> list) {
+    if (list == null || list.isEmpty()) {
+      return "";
+    }
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0, len = list.size(); i < len; i++) {
+      if (i == (len - 1)) {
+        builder.append(list.get(i));
+      } else {
+        builder.append(list.get(i)).append(delimiter);
+      }
+    }
+    return builder.toString();
+  }
+
+  /**
+   * Implementation according to https://www.oracle.com/technetwork/database/enterprise-edition/oraclenetservices-neteasyconnect-133058.pdf
+   *
+   * @param url the url without the oracle jdbc prefix
+   * @return the oracle connection info if the url could be parsed, or null otherwise.
+   */
+  public static OracleConnectionInfo parseEasyConnect(final String url) {
+    final Matcher matcher = EASY_CONNECT_PATTERN.matcher(url);
+    if (matcher.matches()) {
+      final OracleConnectionInfo result = new OracleConnectionInfo();
+      final String host = matcher.group("host");
+      final String portGroup = matcher.group("port");
+      final int dbPort =
+          portGroup != null ? Integer.parseInt(portGroup.substring(1)) : DEFAULT_PORT;
+      result.setDbPeer(host + ":" + dbPort);
+      final String service = matcher.group("service");
+      if (service != null) {
+        result.setDbInstance(service.substring(1));
+      } else {
+        result.setDbInstance(host);
+      }
+      return result;
+    }
+    return null;
+  }
+
+  public static class OracleConnectionInfo {
+    private String dbInstance;
+    private String dbPeer;
+
+    public String getDbInstance() {
+      return dbInstance;
     }
 
-    private OracleConnectionInfo parseTnsName(final String url) {
-        final String hosts = parseDatabaseHostsFromTnsUrl(url);
-        if (hosts != null) {
-            final int idxServiceName = url.indexOf("SERVICE_NAME");
-            final int start = url.indexOf('=', idxServiceName) + 1;
-            final int end = url.indexOf(")", start);
-            final String serviceName = url.substring(start, end);
-            return new OracleConnectionInfo() //
-                .setDbPeer(hosts) //
-                .setDbInstance(serviceName);
-        }
-        return null;
+    public OracleConnectionInfo setDbInstance(final String dbInstance) {
+      this.dbInstance = dbInstance;
+      return this;
     }
 
-    public static String parseDatabaseHostsFromTnsUrl(String url) {
-        int beginIndex = url.indexOf("DESCRIPTION");
-        if (beginIndex == -1) {
-            return null;
-        }
-        List<String> hosts = new ArrayList<String>();
-        do {
-            int hostStartIndex = url.indexOf("HOST", beginIndex);
-            if (hostStartIndex == -1) {
-                break;
-            }
-            int equalStartIndex = url.indexOf("=", hostStartIndex);
-            int hostEndIndex = url.indexOf(")", hostStartIndex);
-            String host = url.substring(equalStartIndex + 1, hostEndIndex);
-
-            int port = DEFAULT_PORT;
-            int portStartIndex = url.indexOf("PORT", hostEndIndex);
-            int portEndIndex = url.length();
-            if (portStartIndex != -1) {
-                int portEqualStartIndex = url.indexOf("=", portStartIndex);
-                portEndIndex = url.indexOf(")", portEqualStartIndex);
-                port = Integer.parseInt(url.substring(portEqualStartIndex + 1, portEndIndex).trim());
-            }
-            hosts.add(host.trim() + ":" + port);
-            beginIndex = portEndIndex;
-        } while (true);
-        return join(",", hosts);
+    public String getDbPeer() {
+      return dbPeer;
     }
 
-    private static String join(String delimiter, List<String> list) {
-        if (list == null || list.isEmpty()) {
-            return "";
-        }
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0, len = list.size(); i < len; i++) {
-            if (i == (len - 1)) {
-                builder.append(list.get(i));
-            } else {
-                builder.append(list.get(i)).append(delimiter);
-            }
-        }
-        return builder.toString();
+    public OracleConnectionInfo setDbPeer(final String dbPeer) {
+      this.dbPeer = dbPeer;
+      return this;
     }
-
-    /**
-     * Implementation according to https://www.oracle.com/technetwork/database/enterprise-edition/oraclenetservices-neteasyconnect-133058.pdf
-     *
-     * @param url the url without the oracle jdbc prefix
-     * @return the oracle connection info if the url could be parsed, or null otherwise.
-     */
-    public static OracleConnectionInfo parseEasyConnect(final String url) {
-        final Matcher matcher = EASY_CONNECT_PATTERN.matcher(url);
-        if (matcher.matches()) {
-            final OracleConnectionInfo result = new OracleConnectionInfo();
-            final String host = matcher.group("host");
-            final String portGroup = matcher.group("port");
-            final int dbPort = portGroup != null ? Integer.parseInt(portGroup.substring(1)) : DEFAULT_PORT;
-            result.setDbPeer(host + ":" + dbPort);
-            final String service = matcher.group("service");
-            if (service != null) {
-                result.setDbInstance(service.substring(1));
-            } else {
-                result.setDbInstance(host);
-            }
-            return result;
-        }
-        return null;
-    }
-
-    public static class OracleConnectionInfo {
-        private String dbInstance;
-        private String dbPeer;
-
-        public String getDbInstance() {
-            return dbInstance;
-        }
-
-        public OracleConnectionInfo setDbInstance(final String dbInstance) {
-            this.dbInstance = dbInstance;
-            return this;
-        }
-
-        public String getDbPeer() {
-            return dbPeer;
-        }
-
-        public OracleConnectionInfo setDbPeer(final String dbPeer) {
-            this.dbPeer = dbPeer;
-            return this;
-        }
-    }
+  }
 }
--- src/test/java/io/opentracing/contrib/jdbc/parser/OracleURLParserTest.java
+++ src/test/java/io/opentracing/contrib/jdbc/parser/OracleURLParserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 The OpenTracing Authors
+ * Copyright 2017-2020 The OpenTracing Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -13,75 +13,77 @@
  */
 package io.opentracing.contrib.jdbc.parser;
 
-import java.util.stream.Stream;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.opentracing.contrib.jdbc.ConnectionInfo;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class OracleURLParserTest {
-    private static final String ORACLE = "oracle";
-    private final OracleURLParser testee = new OracleURLParser();
+  private static final String ORACLE = "oracle";
+  private final OracleURLParser testee = new OracleURLParser();
 
-    private static Stream<Arguments> easyConnectUrls() {
-        return Stream.of( //
-            Arguments.of("jdbc:oracle:thin:@192.168.105.100", "192.168.105.100:1521", "192.168.105.100"), //
-            Arguments.of("jdbc:oracle:thin:@192.168.105.100:1234", "192.168.105.100:1234", "192.168.105.100"), //
-            Arguments.of("jdbc:oracle:thin:@localhost/XEPDB1", "localhost:1521", "XEPDB1"), //
-            Arguments.of("jdbc:oracle:thin:@localhost:1234/XEPDB1", "localhost:1234", "XEPDB1"), //
-            Arguments.of("jdbc:oracle:thin:@//localhost:1234/XEPDB1", "localhost:1234", "XEPDB1"), //
-            Arguments.of("jdbc:oracle:thin:@//localhost:1234/XEPDB1:server/instance", "localhost:1234", "XEPDB1") //
-        );
-    }
+  private static Stream<Arguments> easyConnectUrls() {
+    return Stream.of( //
+        Arguments
+            .of("jdbc:oracle:thin:@192.168.105.100", "192.168.105.100:1521", "192.168.105.100"), //
+        Arguments.of("jdbc:oracle:thin:@192.168.105.100:1234", "192.168.105.100:1234",
+            "192.168.105.100"), //
+        Arguments.of("jdbc:oracle:thin:@localhost/XEPDB1", "localhost:1521", "XEPDB1"), //
+        Arguments.of("jdbc:oracle:thin:@localhost:1234/XEPDB1", "localhost:1234", "XEPDB1"), //
+        Arguments.of("jdbc:oracle:thin:@//localhost:1234/XEPDB1", "localhost:1234", "XEPDB1"), //
+        Arguments.of("jdbc:oracle:thin:@//localhost:1234/XEPDB1:server/instance", "localhost:1234",
+            "XEPDB1") //
+    );
+  }
 
-    private static Stream<Arguments> tnsNameUrls() {
-        return Stream.of( //
-            Arguments
-                .of("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST= localhost )(PORT= 1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))",
-                    "localhost:1521", "orcl"), //
-            Arguments
-                .of("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL= TCP)(HOST=hostA)(PORT= 1523 ))(ADDRESS=(PROTOCOL=TCP)(HOST=hostB)(PORT= 1521 )))(SOURCE_ROUTE=yes)(CONNECT_DATA=(SERVICE_NAME=orcl)))",
-                    "hostA:1523,hostB:1521", "orcl") //
-        );
-    }
+  private static Stream<Arguments> tnsNameUrls() {
+    return Stream.of( //
+        Arguments
+            .of("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST= localhost )(PORT= 1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))",
+                "localhost:1521", "orcl"), //
+        Arguments
+            .of("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL= TCP)(HOST=hostA)(PORT= 1523 ))(ADDRESS=(PROTOCOL=TCP)(HOST=hostB)(PORT= 1521 )))(SOURCE_ROUTE=yes)(CONNECT_DATA=(SERVICE_NAME=orcl)))",
+                "hostA:1523,hostB:1521", "orcl") //
+    );
+  }
 
-    private static Stream<Arguments> otherUrls() {
-        return Stream.of( //
-            Arguments.of("jdbc:oracle:thin:@localhost:orcl", "localhost:1521", "orcl"), //
-            Arguments.of("jdbc:oracle:thin:@localhost:1522:orcl", "localhost:1522", "orcl"), //
-            Arguments.of("jdbc:oracle:thin:@//localhost:1521/orcl", "localhost:1521", "orcl"), //
-            Arguments.of("jdbc:oracle:thin:scott/tiger@myhost:1521:orcl", "myhost:1521", "orcl"), //
-            Arguments.of("jdbc:oracle:thin:@orcl", "orcl:1521", "orcl") //
-        );
-    }
+  private static Stream<Arguments> otherUrls() {
+    return Stream.of( //
+        Arguments.of("jdbc:oracle:thin:@localhost:orcl", "localhost:1521", "orcl"), //
+        Arguments.of("jdbc:oracle:thin:@localhost:1522:orcl", "localhost:1522", "orcl"), //
+        Arguments.of("jdbc:oracle:thin:@//localhost:1521/orcl", "localhost:1521", "orcl"), //
+        Arguments.of("jdbc:oracle:thin:scott/tiger@myhost:1521:orcl", "myhost:1521", "orcl"), //
+        Arguments.of("jdbc:oracle:thin:@orcl", "orcl:1521", "orcl") //
+    );
+  }
 
-    @ParameterizedTest(name = "[{0}]")
-    @MethodSource("easyConnectUrls")
-    void parseEasyConnect(final String url, final String dbPeer, final String dbInstance) {
-        final ConnectionInfo result = testee.parse(url);
-        assertThat(result.getDbType()).isEqualTo(ORACLE);
-        assertThat(result.getDbPeer()).isEqualTo(dbPeer);
-        assertThat(result.getDbInstance()).isEqualTo(dbInstance);
-    }
+  @ParameterizedTest(name = "[{0}]")
+  @MethodSource("easyConnectUrls")
+  void parseEasyConnect(final String url, final String dbPeer, final String dbInstance) {
+    final ConnectionInfo result = testee.parse(url);
+    assertThat(result.getDbType()).isEqualTo(ORACLE);
+    assertThat(result.getDbPeer()).isEqualTo(dbPeer);
+    assertThat(result.getDbInstance()).isEqualTo(dbInstance);
+  }
 
-    @ParameterizedTest(name = "[{0}]")
-    @MethodSource("tnsNameUrls")
-    void parseTnsName(final String url, final String dbPeer, final String dbInstance) {
-        final ConnectionInfo result = testee.parse(url);
-        assertThat(result.getDbType()).isEqualTo(ORACLE);
-        assertThat(result.getDbPeer()).isEqualTo(dbPeer);
-        assertThat(result.getDbInstance()).isEqualTo(dbInstance);
-    }
+  @ParameterizedTest(name = "[{0}]")
+  @MethodSource("tnsNameUrls")
+  void parseTnsName(final String url, final String dbPeer, final String dbInstance) {
+    final ConnectionInfo result = testee.parse(url);
+    assertThat(result.getDbType()).isEqualTo(ORACLE);
+    assertThat(result.getDbPeer()).isEqualTo(dbPeer);
+    assertThat(result.getDbInstance()).isEqualTo(dbInstance);
+  }
 
-    @ParameterizedTest(name = "[{0}]")
-    @MethodSource("otherUrls")
-    void parseOther(final String url, final String dbPeer, final String dbInstance) {
-        final ConnectionInfo result = testee.parse(url);
-        assertThat(result.getDbType()).isEqualTo(ORACLE);
-        assertThat(result.getDbPeer()).isEqualTo(dbPeer);
-        assertThat(result.getDbInstance()).isEqualTo(dbInstance);
-    }
+  @ParameterizedTest(name = "[{0}]")
+  @MethodSource("otherUrls")
+  void parseOther(final String url, final String dbPeer, final String dbInstance) {
+    final ConnectionInfo result = testee.parse(url);
+    assertThat(result.getDbType()).isEqualTo(ORACLE);
+    assertThat(result.getDbPeer()).isEqualTo(dbPeer);
+    assertThat(result.getDbInstance()).isEqualTo(dbInstance);
+  }
 }
