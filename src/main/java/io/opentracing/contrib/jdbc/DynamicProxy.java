58a9ffc430986fce22d9d4257dba6b81a011937c
Seva Safris <seva@safris.org>
2020-01-15 12:29:50 +0700

    Fix to ClassCastException when using JPA

--- src/main/java/io/opentracing/contrib/jdbc/DynamicProxy.java
+++ src/main/java/io/opentracing/contrib/jdbc/DynamicProxy.java
@@ -16,16 +16,41 @@ package io.opentracing.contrib.jdbc;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Seva Safris
  */
 public class DynamicProxy {
+  private static void recurse(final Class<?> iface, final Set<Class<?>> set) {
+    if (set.contains(iface))
+      return;
+
+    set.add(iface);
+    for (final Class<?> extended : iface.getInterfaces())
+      recurse(extended, set);
+  }
+
+  static Class<?>[] getAllInterfaces(final Class<?> cls) {
+    Class<?> next = cls;
+    final Set<Class<?>> ifaces = new HashSet<>();
+    do {
+      for (final Class<?> iface : next.getInterfaces())
+        recurse(iface, ifaces);
+
+      next = next.getSuperclass();
+    }
+    while (next != null);
+
+    return ifaces.toArray(new Class[ifaces.size()]);
+  }
+
   @SuppressWarnings("unchecked")
   public static <T> T wrap(final T target, final T wrapper) {
     final Class<?> cls = target.getClass();
     return (T) Proxy
-        .newProxyInstance(cls.getClassLoader(), cls.getInterfaces(), new InvocationHandler() {
+        .newProxyInstance(cls.getClassLoader(), getAllInterfaces(cls), new InvocationHandler() {
           @Override
           public Object invoke(final Object proxy, final Method method, final Object[] args)
               throws Throwable {
